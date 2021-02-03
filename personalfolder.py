3e59e25957bcccf56f1765cb4ccc9693cf06c0cf
Your Name <you@example.com>
2021-02-03 10:51:57 +0100

    added personal branch

---
+++ personalfolder.py
@@ -0,0 +1,306 @@
+#print ("HELLO world.!!!")
+#print ("welcome to my code")
+#print ("my age")
+#print ("can i get access to my bank? ")
+#print ("i want to be the best and beat the beat")
+#
+#x= 10 + 3 * 2
+#x= 10 + 3 * 2
+#print (x)
+#x= (10*3+7)
+#print(x)
+#age=23
+#print ("age")
+#m= 10 + 10 * 30 
+# #print (x)
+# print ("hello world")
+#print ("Good Afternoon sir, this is to tell you that we are open for business")
+#x = 10 + 10 
+#print (x)
+#name=input("10") 
+#print (name)
+#favorite_color = ("what is my favorite color? ")
+#print (favorite_color)
+#name = ("what i smy name ? ")
+#print (name)
+
+#print("hello world")
+#print ("what is my name ? ")
+#print ("my age")
+
+# print("0----")
+# print ("!!!!!")
+# print ("*" * 10)
+# new_client = ('kunleade')
+# age = 20 
+# is_new = True 
+# print (age)
+
+# # #shirt =input ("how much do you dry clean a shirt? " )
+# #print ("hi"+ shirt)
+
+
+
+# import sys
+# print ("python version")
+# print (sys.version)
+
+
+
+#shirt =input ("how much do you dry clean a shirt? " )
+#print ("hi"+ shirt)
+
+#shirt =input ("how much do you dry clean a shirt? " )
+#print ("hi"+ shirt)
+
+#shirt =input ("how much do you dry clean a shirt? " )
+#print ("hi"+ shirt)
+
+#shirt =input ("how much do you dry clean a shirt? " )
+#print ("hi"+ shirt)
+
+#name = input ("what is your name ? ")
+#favourite_color = input ( "what is your favorite color? ")
+#print( name + "like" favourite_color)
+
+#name = input("what is your name ? ")
+#favorite_color = input ("what is your favorite color")
+#print (name + " likes " + favorite_color) 
+
+
+
+# import datetime
+
+# now = datetime.datetime.now()
+# print ("current date and time: ")
+# print (now.strftime("01-28-2021. 3:33pm"))
+
+
+
+
+
+
+#birth_year = input ("what is my birth year ? ")
+#age = 2021 - int(birth_year)
+#print (age)
+#x=(2+4) * 10-3
+#print(x)
+#x = 10 + 2 ** 2
+#print (x)
+#x = (10+7) * 5 ** 5
+#print (x)
+#name = input ("what is your name?"  )
+#age = input ("how old are you ?" )
+#print (name + " and " + age ) 
+
+#x = {10, 3, 3, 7, 8, 0,}
+#print(x)
+
+#print    ("O*")
+#print   ("***")
+#print   ("****")
+#print   ("*****")
+#print  ("******") 
+
+#c = (1,11,2,3,"a"."b")
+#print(sorted (c))
+#x = (5.76543)
+#print(round(x))
+
+#c = ("j","k","l","m","n","b")
+#x = sorted (c, reverse=True)
+#print(x)
+#x = round (5.76543,) 
+#print(x)
+# print ( "my name is michael end= " ",)
+# print ("i am from ondo")
+
+#from time import sleep 
+
+# print ("1")
+# sleep (2)
+# print("2")
+
+# q = (2,3,5, 5,4,7,7,)
+# print (sum(q))
+
+# name = input ("Please enter your name.: ")
+# age = input ("please enter your age.: " )
+# hobby1 = input ("please enter hobby.: ")
+# hobby2 = input ("please enter another hobbies.: ")
+
+# print (name)
+# print (age)
+# print (hobby1)
+# print (hobby2)
+
+# print("\n", name.title (), "is", age, "years.old. \nhobbies.are listed below")
+# print ("\t-", hobby1)
+# print ("\t-", hobby2)  
+# print("\n", name.title (), "is", age, "years.old. \nhobbies.are listed below")
+# print ("\t-", hobby1)
+# print ("\t-", hobby2)  
+
+
+#tripple quoted strings
+
+# x = """i am a very sincere person, i will like to meet people of like passion 
+# i trust God for my bills and need 
+# God is faithful 
+# i believe GOD
+#           BEST REGARDS
+#           STORY FOR THE GOD"""
+# print (x)
+
+# x = (1,8,3,9,10,11,23,45,100,)
+# print(min (x))
+# print (max(x))
+# print (sorted (x))
+# print (sorted(x,reverse=True))
+# x = range(2,15,)
+# print (list(x))
+
+
+# from time import sleep 
+
+# print ("***")
+# sleep (5)
+# print ("****")
+# sleep (7)
+# print ("####")
+# sleep (9)
+# print ("$$$")
+
+# # print (10 + 12)
+# x = (" A", "B", "C", "D", "E", "F")
+# print (sorted (x, reverse=True))
+# x = (22900 + 22450 + 48050)
+# print (x)
+
+# paid = (2200 + 1350 + 1200 + 1500 + 400 + 1500 + 1100 + 1000 + 1000 + 400 + 1350 + 1300 + 900 + 1400 
+# + 600 + 1500 + 500 + 400 + 4200 + 1600 + 500 + 2270 + 1000 + 1000 + 400 + 3900 + 700 + 2800 + 900 + 1400 + 3600 + 2200 + 3350 +1000 + 1500 +750)
+# print (paid)
+# unpaid = (1400, 1400, 600, 250, 200, 100, 9500, 300, 4100, 2250, 2800, 4100,5000, 2500, 1300, 1250, 1100, 1200, 1000,200,200,)
+# print (sum (unpaid))
+
+
+# name = input ( "what is my name ? ")
+# age = input ( " how old are you ? ")
+# nature_of_business = input ("what kind of business do you engage in ? ")
+# home_address = input ("your home address? ")
+# status = input ( " what is your status ? ")
+# Gender = input ("your Gender? ")
+# print (name)
+# print (age)
+# print (nature_of_business)
+# print (home_address)
+# print (status)
+# print (Gender)
+
+# print ("WELCOME TO MY WORLD CHECK IF THIS LETTER ENDS WITH LATTER (e).")
+
+# word = input ( "please enter a word: ").lower()
+# print(word.endswith("e"))
+
+# x = input ("please enter my name? ").upper()
+# print (x.startswith("M"))
+
+
+
+# age = input ("please enter your age ? ")
+# x = (int(age) % 2) 
+# message = "my_number is Even: " + str (not bool (x))
+# print (message)
+
+# p = int(input ("please enter a value of P: ? "))
+# r = int(input ("please enter a vlues of R: "))
+# t = int(input ("please enter a vlues of T: "))
+# i = (p * r * t) /100
+# message  =  f"if you take a loam of {p:,} at the rate of {r}% for a period of {t:,} month you will pay back {i:,} as interest"
+# print (i)
+# print (message)
+
+
+# p = float(input ("please enter a value of P: ? "))
+# r = float(input ("please enter a vlues of R: "))
+# t = float(input ("please enter a vlues of T: "))
+# i = (p * r * t) /100
+# message  =  f"if you take a loam of {p:,} at the rate of {r}% for a period of {t:,} month you will pay back {i:,} as interest"
+# print (i)
+# print (message)
+
+# name = "Jennfier"
+# print (name [1:-1])
+# course = 'new to python'
+# print ('python' in course)
+
+# is_hot = False
+# is_cold = True
+
+# if is_hot:
+#      print("its a hot day")
+#      print ("wear light cloth")
+# else:
+#     print ("it's a cold day")
+#     print ("drink hot tea")
+# print ("have fun")
+
+# price = 1000000
+# has_good_credit = False
+
+# if has_good_credit:
+#     down_payment = price * 0.1
+# else:
+#     down_payment = price * 0.2
+# print (down_payment)
+# ASSIGNMENT
+import cmath
+
+# # "ax ** 2 + bx + c" 
+
+# a = 5
+# b = 6
+# c = 1
+#  x= (b**2) - (4*a*c)
+
+# equ1 = (-b-cmath.sqrt(x))/ (2*a)
+# equ2 = (-b+cmath.sqrt(x))/ (2*a)
+# print ("{0},{1}".format(equ1,equ2))
+
+
+
+
+
+# a = float (input("enter value of a"))
+# b = float (input("enter value of b"))
+# c = float (input("enter value of c"))
+# x= (b**2) - (4*a*c)
+
+# equ1 = (-b-cmath.sqrt(x))/ (2*a)
+# equ2 = (-b+cmath.sqrt(x))/ (2*a)
+# print ("{0},{1}".format(equ1,equ2))
+
+# pythagora forumlar
+ 
+# import square function from math
+# a2 + b2 = c2 
+# a = 3
+# b = 4
+# c = 0
+# number = "3 ** 0.5 + 4 ** 0.5 = c ** 0.5"
+# print (number)
+
+# a = float (input("enter value of a"))
+# b = float (input("enter value of b"))
+# c = float (input("enter value of c"))
+# x= (b**2) - (4*a*c)
+
+# equ1 = (-b- (x**0.5))/ (2*a)
+# equ2 = (-b+ (x**0.5))/ (2*a)
+# print ("{0},{1}".format(equ1,equ2))
+
+a = int (input("enter the value of  a ? "))
+b = int ( input("enter the value of b ?"))
+z = (a ** 2) + (b ** 2 )
+to_get_our_c = z ** 0.5
+print (to_get_our_c)
